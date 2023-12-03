3a4900cf712f8a739a6b3caa8e183ef1cab7bbd0
VasylEpov <vepov86@gmail.com>
2023-12-03 14:37:59 +0200

    AddCosts

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import CostItem from 'components/CostItem';
+
+export const App = () => {
+  const costs = [
+    {
+      date: new Date(2021, 2, 12),
+      description: 'Холодильник',
+      amount: 999.99,
+    },
+    {
+      date: new Date(2021, 11, 25),
+      description: 'MacBook',
+      amount: 1254.72,
+    },
+    {
+      date: new Date(2021, 11, 25),
+      description: 'Джинси',
+      amount: 49.99,
+    },
+  ];
+
+  return (
+    <div>
+      <h1>React homework template</h1>
+      <CostItem
+        date={costs[0].date}
+        description={costs[0].description}
+        amount={costs[0].amount}
+      ></CostItem>
+      <CostItem
+        date={costs[1].date}
+        description={costs[1].description}
+        amount={costs[1].amount}
+      ></CostItem>
+      <CostItem
+        date={costs[2].date}
+        description={costs[2].description}
+        amount={costs[2].amount}
+      ></CostItem>
+    </div>
+  );
+};
